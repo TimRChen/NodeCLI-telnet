d26abfd51b5f306409e279b1b054b365a00134e8
Tim <timrchen@foxmail.com>
2017-08-18 12:08:02 +0800

    增加后台对话显示

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ const net = require('net');
 let count = 0;
 let users = {};
 
-let server = net.createServer(function (conn) {
+let server = net.createServer(function (socket) {
     count++;
-    conn.setEncoding('utf8');
     let nickname;
-
+    socket.setEncoding('utf8');    
     // 页面tip
-    conn.write(
-        '\n > welcome to \033[92mnode-chat\033[39m!'
+    socket.write(
+        '\n > welcome to \033[92mgroup-chat\033[39m!'
         + '\n > ' + count + ' other people are connected at this time.'
         + '\n > please write your name and press enter: '
     );
@@ -31,39 +30,56 @@ let server = net.createServer(function (conn) {
     };
 
     // 监听用户行为作出处理
-    conn.on('data', function (data) {
+    socket.on('data', function (chunk) {
+        socket.setEncoding('utf8');
         // 删除回车符
-        data = data.replace('\r\n', '');
+        chunk = chunk.replace('\r\n', '');
         if (!nickname) {
-            if (users[data]) {
-                conn.write('\033[93m> nickname already in use. try again:\033[39m ');
+            if (users[chunk]) {
+                socket.write('\033[93m> nickname already in use. try again:\033[39m ');
                 return;
             } else {
-                nickname = data;
-                // 将conn对象赋予用户，赋予用户可操作权限
-                users[nickname] = conn;
-
+                nickname = chunk;
+                // 将socket对象赋予用户，赋予用户可操作权限
+                users[nickname] = socket;
+                console.log('\033[90m > ' + nickname + ' joined the room\033[39m\n');
                 broadcast('\033[90m > ' + nickname + ' joined the room\033[39m\n');
             }
         } else {
-            // 验证用户为已注册，则输入数据(data)为聊天信息
+            // 验证用户为已注册，则输入数据(chunk)为聊天信息
             for (var i in users) {
                 if (i != nickname) {
-                    broadcast('\033[96m > ' + nickname + ':\033[39m ' + data + '\n', true);
+                    console.log('\033[96m > ' + nickname + ':\033[39m ' + chunk + '\n')
+                    broadcast('\033[96m > ' + nickname + ':\033[39m ' + chunk + '\n', true);
                 }
             }
         }
     });
 
+    socket.on('error', function (err) {
+        console.log(err)
+    });
+
 
     // 当其中某个用户断开连接时，需要清除数据
-    conn.on('close', function () {
+    socket.on('close', function () {
         count--;
         broadcast('\033[90m > ' + nickname + ' left the room\033[39m\n');
         delete users[nickname];
     });
 });
 
-server.listen(3000, function () {
-    console.log('\033[96m   server listening on *:3000\033[39m');
+server.listen(8080, function () {
+    console.log('\033[96m   server listening on *:8080\033[39m', server.address());
 });
+
+
+server.on('error', (e) => {
+    if (e.code === 'EADDRINUSE') {
+      console.log('Address in use, retrying...');
+      setTimeout(() => {
+        server.close();
+        server.listen(8080);
+      }, 1000);
+    }
+  });
